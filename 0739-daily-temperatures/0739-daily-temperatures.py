873a1ff5d83e70746ce86a8e94482e470e2f177c
Kevin Hoang <109319259+knhoang10@users.noreply.github.com>
2023-07-09 22:05:13 -0400

    Time: 9637 ms (5.02%), Space: 31 MB (61.02%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,23 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = []
+        stack = []
+        tempExist = False
+        for i in range(len(temperatures)-1, -1, -1):
+            print(stack)
+            for j in range(len(stack)-1, -1, -1):
+                if temperatures[i] < temperatures[stack[j]]:
+                    stack.append(i)
+                    tempExist = True
+                    result.insert(0, stack[j]-i)
+                    break
+                else:
+                    stack.pop(len(stack) - 1)
+                    
+            if tempExist == False:
+                result.insert(0, 0)
+                stack.append(i)
+            else:
+                tempExist = False
+        
+        return result
