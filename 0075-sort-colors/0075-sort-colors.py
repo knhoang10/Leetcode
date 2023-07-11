cb6ed32db06ee4c964ba0d1f9a747159ce635604
Kevin Hoang <109319259+knhoang10@users.noreply.github.com>
2023-07-11 13:42:08 -0400

    Time: 48 ms (67.26%), Space: 16.3 MB (73.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,20 +4,38 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        # Count how many 0, 1, 2
-        # Loop through 0, 1, 2 and replace numbers in num
-        numsDict = {
-            0:0,
-            1:0,
-            2:0
-        }
-        for i in range(len(nums)):
-            numsDict[nums[i]] += 1
+#         # Count how many 0, 1, 2
+#         # Loop through 0, 1, 2 and replace numbers in num
+#         numsDict = {
+#             0:0,
+#             1:0,
+#             2:0
+#         }
+#         for i in range(len(nums)):
+#             numsDict[nums[i]] += 1
         
-        result = [0] * numsDict[0] + [1] * numsDict[1] + [2] * numsDict[2]
+#         result = [0] * numsDict[0] + [1] * numsDict[1] + [2] * numsDict[2]
 
-        for i in range(len(nums)):
-            nums[i] = result[i]
-                
+#         for i in range(len(nums)):
+#             nums[i] = result[i]
+            
+        def switch(a, b):
+            temp = nums[a]
+            nums[a] = nums[b]
+            nums[b] = temp
+            
+        # Two pointer
+        left = 0
+        right = len(nums) - 1
+        i = 0
         
+        while i <= right:
+            if nums[i] == 0:
+                switch(left, i)
+                left += 1
+            elif nums[i] == 2:
+                switch(right, i)
+                right -= 1
+                i -= 1
+            i += 1
         
